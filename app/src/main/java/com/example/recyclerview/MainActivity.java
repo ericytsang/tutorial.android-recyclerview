f1b3d542bc28e05ebab6be48620bf39bc8e07b34
Eric Tsang <surplus.et@gmail.com>
2016-10-26 04:26:37 -0700

    dynamic recycler view
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -4,24 +4,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+    private CustomAdapter recyclerViewAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerViewAdapter = new CustomAdapter();
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview_quotes);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new CustomAdapter());
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+        case R.id.action_add:
+            recyclerViewAdapter.addRandom();
+            return true;
+        case R.id.action_remove:
+            recyclerViewAdapter.removeRandom();
+            return true;
+        case R.id.action_shuffle:
+            recyclerViewAdapter.shuffleExisting();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     class CustomAdapter extends RecyclerView.Adapter<QuoteViewHolder>
     {
-        private String[] quotes = getResources().getStringArray(R.array.quotes);
+        private String[] quotePool = getResources().getStringArray(R.array.quotes);
+
+        private List<String> quotes = new ArrayList<>();
 
         @Override
         public QuoteViewHolder onCreateViewHolder(ViewGroup parent,int viewType)
@@ -32,13 +70,37 @@ public class MainActivity extends AppCompatActivity
         @Override
         public void onBindViewHolder(QuoteViewHolder holder,int position)
         {
-            holder.setModel(quotes[position]);
+            holder.setModel(quotes.get(position));
         }
 
         @Override
         public int getItemCount()
         {
-            return quotes.length;
+            return quotes.size();
+        }
+
+        public void addRandom()
+        {
+            int listIndex = (int) Math.floor((1-Math.random())*quotes.size());
+            int quotePoolIndex = (int) Math.floor((1-Math.random())*quotePool.length);
+            quotes.add(listIndex,quotePool[quotePoolIndex]);
+            notifyDataSetChanged();
+        }
+
+        public void shuffleExisting()
+        {
+            Collections.shuffle(quotes);
+            notifyDataSetChanged();
+        }
+
+        public void removeRandom()
+        {
+            if (!quotes.isEmpty())
+            {
+                int listIndex = (int) Math.floor((1-Math.random())*quotes.size());
+                quotes.remove(listIndex);
+                notifyDataSetChanged();
+            }
         }
     }
 }
